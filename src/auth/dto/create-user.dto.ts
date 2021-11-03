1c4f1be26c7c3bbcb79e461e422dc4eb8effc6ac
vitoraa <vitor.lima@outlook.com>
2021-11-03 14:28:18 -0300

    renaming dtos

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,5 +1,4 @@
 export class CreateUserDto {
-  id: string;
   userName: string;
   password: string;
   email: string;
--- src/auth/dto/update-user.dto.ts
+++ src/auth/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends OmitType(CreateUserDto, ['id'] as const) { }
+export class UpdateUserDto extends PartialType(CreateUserDto) { }
