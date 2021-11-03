cf90141cab5494aff9643335b56bc71f37f7eadf
vitoraa <vitor.lima@outlook.com>
2021-11-03 15:24:04 -0300

    using class validator

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   userName: string;
+  @IsNotEmpty()
   password: string;
+  @IsNotEmpty()
+  passwordConfirmation: string;
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
   role: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { enviroment } from './enviroment';
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(enviroment.PORT);
 }
 bootstrap();
