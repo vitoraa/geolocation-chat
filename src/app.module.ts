cde0809702cdb35aacffff4f906ad823dbfcd5cc
vitoraa <vitor.lima@outlook.com>
2021-11-05 09:34:55 -0300

    created user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ormConfig } from './infra/database/config/ormconfig';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormConfig()),
-    AuthModule],
+    AuthModule,
+    UsersModule],
   controllers: [],
   providers: [],
 })
