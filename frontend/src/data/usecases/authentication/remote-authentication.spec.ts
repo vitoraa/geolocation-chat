bcb5ee7dc4ea79fb6a2c6b2a3bdd095993677cca
vitoraa <vitor.lima@outlook.com>
2021-11-08 16:08:26 -0300

    mock to its own file

--- frontend/src/data/usecases/authentication/remote-authentication.spec.ts
+++ frontend/src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,14 +1,8 @@
-import { HttpPostClient } from "../../protocols/http/http-post-client"
+import { HttppostClientSpy } from "../../test/mock-http-client"
 import { RemoteAuthentication } from "./remote-authentication"
 
 describe('RemoteAuthentication', () => {
   test('Should call HttppostClient with correct URL', async () => {
-    class HttppostClientSpy implements HttpPostClient {
-      url?: string
-      async post (url: string): Promise<void> {
-        this.url = url
-      }
-    }
     const url = 'any_url'
     const httppostClient = new HttppostClientSpy()
     const sut = new RemoteAuthentication(url, httppostClient)
