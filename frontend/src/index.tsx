93a544eecf602d2df5c3aa329b9f4b0b0de3741f
vitoraa <vitor.lima@outlook.com>
2021-11-09 17:38:55 -0300

    render Router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Router from './main/factories/routes/routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router />,
   document.getElementById('root')
 );
 
