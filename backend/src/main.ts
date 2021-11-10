1ee7fa39a459073cf8c68559ee131d0d5323565e
vitoraa <vitor.lima@outlook.com>
2021-11-10 16:44:20 -0300

    set Swagger Module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { environment } from './environment';
 
@@ -7,6 +8,14 @@ async function bootstrap () {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Geolocation Chat')
+    .setDescription('Geolocation Chat API')
+    .setVersion('0.1')
+    .addTag('geolocation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(environment.port);
 }
 bootstrap();
