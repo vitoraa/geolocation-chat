ffd7dcbddfbca6715d2ce21d90e663a455b2b197
vitoraa <vitor.lima@outlook.com>
2021-11-10 11:41:27 -0300

    should call authservice with corrects params on Login

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserRoles } from '../shared/user-roles';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+const makeFakeRequest = (): CreateUserDto => ({
+  email: 'any_email@email.com',
+  password: 'any_password',
+  passwordConfirmation: 'any_password',
+  name: 'any name',
+  role: UserRoles.USER
+})
+
 describe('AuthController', () => {
   let controller: AuthController;
-  let service: AuthService;
+  let spyService: AuthService;
 
   beforeEach(async () => {
+
+    const AuthServiceProvider = {
+      provide: AuthService,
+      useFactory: () => ({
+        login: jest.fn(() => null),
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthServiceProvider],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
-    service = module.get<AuthService>(AuthService);
+    spyService = module.get<AuthService>(AuthService);
+  });
+
+  test('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('login', () => {
+    test('should call AuthService with corrects params', async () => {
+      const loginSpy = jest.spyOn(spyService, 'login')
+      controller.login(makeFakeRequest())
+      expect(loginSpy).toHaveBeenCalledWith(makeFakeRequest())
+    });
   });
 });
