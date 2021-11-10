787005941381cbcc115d36664ec59569af4c2405
vitoraa <vitor.lima@outlook.com>
2021-11-10 17:22:28 -0300

    api response to document on Swagger

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginUserDto } from '../users/dto/login-user.dto';
 import { AuthService } from './auth.service';
@@ -7,12 +8,16 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor (private readonly authService: AuthService) { }
 
+  @ApiResponse({ status: 201, description: 'User created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Post('/signup')
   createUser (@Body() user: CreateUserDto) {
     const response = this.authService.create(user);
     return response;
   }
 
+  @ApiResponse({ status: 200, description: 'Login done' })
+  @ApiResponse({ status: 401, description: 'Login invalid' })
   @HttpCode(200)
   @Post('/login')
   login (@Body() loginUserDto: LoginUserDto) {
